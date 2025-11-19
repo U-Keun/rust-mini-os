2b897d0b1ab75d2b015358038e19c2b767dbbd1c
u-keun song <u-keunsong@u-keunui-MacBookAir-2.local>
2025-11-19 16:41:12 +0900

    refactor: revise addr.rs

--- src/addr.rs
+++ src/addr.rs
@@ -6,24 +6,31 @@ pub struct PAddr(pub usize);
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
 pub struct VAddr(pub usize);
 
-impl PAddr {
-    #[inline] pub const fn align_up(self, align: usize) -> Self {
+pub trait Addr: Copy + Eq + Ord {
+    fn raw(self) -> usize;
+    fn from_raw(x: usize) -> Self;
+
+    #[inline]
+    fn align_up(self, align: usize) -> Self {
         debug_assert!(align.is_power_of_two());
-        PAddr((self.0 + align - 1) & !(align - 1))
+        let x = self.raw();
+        let y = (x + align - 1) & !(align - 1);
+        Self::from_raw(y)
     }
-    #[inline] pub const fn is_aligned(self, align: usize) -> bool {
+
+    #[inline]
+    fn is_aligned(self, align: usize) -> bool {
         debug_assert!(align.is_power_of_two());
-        (self.0 & (align - 1)) == 0
+        (self.raw() & (align - 1)) == 0
     }
 }
 
-impl VAddr {
-    #[inline] pub const fn align_up(self, align: usize) -> Self {
-        debug_assert!(align.is_power_of_two());
-        VAddr((self.0 + align - 1) & !(align - 1))
-    }
-    #[inline] pub const fn is_aligned(self, align: usize) -> bool {
-        debug_assert!(align.is_power_of_two());
-        (self.0 & (align - 1)) == 0
-    }
+impl Addr for PAddr {
+    #[inline] fn raw(self) -> usize { self.0 }
+    #[inline] fn from_raw(x: usize) -> Self { PAddr(x) }
+}
+
+impl Addr for VAddr {
+    #[inline] fn raw(self) -> usize { self.0 }
+    #[inline] fn from_raw(x: usize) -> Self { VAddr(x) }
 }
